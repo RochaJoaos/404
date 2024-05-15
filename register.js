215b3750873956fa1acf355b1cef48577f4dc2ba
João Henrique <souza.joaohenrique.rocha@gmail.com>
2024-05-14 21:40:38 -0300

    backgrond log 404

--- register.js
+++ register.js
@@ -4,9 +4,19 @@ function registerUser(){
     const password = document.getElementById("input-password")
     const confirm = document.getElementById("input-confirm")
 
+    const alertName = document.getElementById("alert-name")
+    const alertEmail = document.getElementById("alert-email")
+    const alertPassword = document.getElementById("alert-password")
+    const alertConfirm = document.getElementById("alert-confirm")
+
+    if(password.value.length < 8){
+        password.style.border = "1px solid var(--alert-color)"
+        alertPassword.innerHTML = "Use 8 caracteres ou mais para sua senha"
+    }
+
+
     if(confirm.value != password.value){
-        confirm.style.border = "2px solid red"
-        document.getElementById("alert-confirm").style.color = "red"
-        document.getElementById("alert-confirm").innerHTML = "Senha incorreta"
+        confirm.style.border = "1px solid var(--alert-color)"
+        alertConfirm.innerHTML = "As senhas não são iguais. Tente novamente."
     }
 }
