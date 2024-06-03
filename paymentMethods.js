a6dc5959609207e5991fd4f66faab66d12879e5d
João Henrique <souza.joaohenrique.rocha@gmail.com>
2024-06-03 20:26:59 -0300

    ae

---
+++ paymentMethods.js
@@ -0,0 +1,8 @@
+function debit(){
+    const debitButton = document.getElementById("debit-btn")
+    const creditButton = document.getElementById("credit-btn")
+    const pixButton = document.getElementById("pix-btn")
+    const boletoButton = document.getElementById("boleto-btn")
+
+    
+}
