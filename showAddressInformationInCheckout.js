f6e0434193ae22afe7936ee7048f904b486db866
João Henrique <souza.joaohenrique.rocha@gmail.com>
2024-06-10 21:54:45 -0300

    update

--- showAddressInformationInCheckout.js
+++ showAddressInformationInCheckout.js
@@ -9,56 +9,58 @@ function showAddress(){
     if(name.value != false && state.value != false && city.value != false && district.value != false && street.value != false && n.value != false){
         document.getElementById("p-cep").innerHTML = document.getElementById("cep").value
         document.getElementById("p-price").innerHTML = "R$ 13,09"
+        document.getElementById("body-address").style.display = "none"
+        document.getElementById("address").style.border = "2px solid green"
+        document.getElementById("address").style.color = "green"
     } else{
+            
+        // NAME - Validação do Campo
+        if(name.value != true){
+            name.style.border = "1px solid var(--alert-color)"      
+        }
+        if(name.value != false){
+            name.style.border = "1px solid var(--color-01)" 
+        }
+
+        // STATE - Validação do Campo    
+        if(state.value != true){
+            state.style.border = "1px solid var(--alert-color)"      
+        }
+        if(state.value != false){
+            state.style.border = "1px solid var(--color-01)"   
+        }
+
+        // CITY - Validação do Campo 
+        if(city.value != true){
+            city.style.border = "1px solid var(--alert-color)"      
+        }
+        if(city.value != false){
+            city.style.border = "1px solid var(--color-01)"   
+        }
+
+        // DISTRICT - Validação do Campo 
+        if(district.value != true){
+            district.style.border = "1px solid var(--alert-color)"      
+        }
+        if(district.value != false){
+            district.style.border = "1px solid var(--color-01)"
+        }
         
-    // NAME - Validação do Campo
-    if(name.value != true){
-        name.style.border = "1px solid var(--alert-color)"      
-    }
-    if(name.value != false){
-        name.style.border = "1px solid var(--color-01)" 
-    }
-
-    // STATE - Validação do Campo    
-    if(state.value != true){
-        state.style.border = "1px solid var(--alert-color)"      
-    }
-    if(state.value != false){
-        state.style.border = "1px solid var(--color-01)"   
-    }
-
-    // CITY - Validação do Campo 
-    if(city.value != true){
-        city.style.border = "1px solid var(--alert-color)"      
-    }
-    if(city.value != false){
-        city.style.border = "1px solid var(--color-01)"   
-    }
-
-    // DISTRICT - Validação do Campo 
-    if(district.value != true){
-        district.style.border = "1px solid var(--alert-color)"      
-    }
-    if(district.value != false){
-        district.style.border = "1px solid var(--color-01)"
-    }
-    
-    // STREET - Validação do Campo 
-    if(street.value != true){
-        street.style.border = "1px solid var(--alert-color)"      
-    }
-    if(street.value != false){
-        street.style.border = "1px solid var(--color-01)"
-    }
-    
-    // NUMBER - Validação do Campo 
-    if(n.value != true){
-        n.style.border = "1px solid var(--alert-color)"      
-    }
-    if(n.value != false){
-        n.style.border = "1px solid var(--color-01)"
-    }
-    
+        // STREET - Validação do Campo 
+        if(street.value != true){
+            street.style.border = "1px solid var(--alert-color)"      
+        }
+        if(street.value != false){
+            street.style.border = "1px solid var(--color-01)"
+        }
+        
+        // NUMBER - Validação do Campo 
+        if(n.value != true){
+            n.style.border = "1px solid var(--alert-color)"      
+        }
+        if(n.value != false){
+            n.style.border = "1px solid var(--color-01)"
+        }   
     }
 
 }
