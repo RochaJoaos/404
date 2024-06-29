92f77b5fee0eb6448cae1174a317b17c3caba057
João Henrique <125095098+RochaJoaos@users.noreply.github.com>
2024-06-29 19:56:35 -0300

    full

--- script/searchCEP.js
+++ script/searchCEP.js
@@ -1,18 +1,11 @@
 function searchCEP(){
-    const cep = document.getElementById("cep")
+    const cep = document.getElementById("cep").value
     const state = document.getElementById("state")
     const city = document.getElementById("city")
     const district = document.getElementById("district")
     const street = document.getElementById("street")
 
-    if(cep.value != true){
-        cep.style.border = "1px solid var(--alert-color)"
-    }
-    if(cep.value != false){
-        cep.style.border = "1px solid var(--color-01)"
-    }
-
-    switch(cep.value){
+    switch(cep){
         case `89226320`:
             state.value = "Santa Catarina"
             city.value = "Joinville"
